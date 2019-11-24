53743bb623fc1b971ee19bedcdcfa7d66a1f7ad7
Krzysiek101 <46795635+Krzysiek101@users.noreply.github.com>
2019-11-24 15:11:00 +0100

    test commit

---
+++ GraWisielec/wisielec/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace wisielec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Litera1 = new System.Windows.Forms.Label();
+            this.Litera2 = new System.Windows.Forms.Label();
+            this.Litera3 = new System.Windows.Forms.Label();
+            this.Litera4 = new System.Windows.Forms.Label();
+            this.Litera5 = new System.Windows.Forms.Label();
+            this.Litera6 = new System.Windows.Forms.Label();
+            this.Litera7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Litera1
+            // 
+            this.Litera1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera1.Location = new System.Drawing.Point(12, 264);
+            this.Litera1.Name = "Litera1";
+            this.Litera1.Size = new System.Drawing.Size(72, 84);
+            this.Litera1.TabIndex = 2;
+            this.Litera1.Text = "_";
+            // 
+            // Litera2
+            // 
+            this.Litera2.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera2.Location = new System.Drawing.Point(90, 264);
+            this.Litera2.Name = "Litera2";
+            this.Litera2.Size = new System.Drawing.Size(75, 84);
+            this.Litera2.TabIndex = 3;
+            this.Litera2.Text = "_";
+            // 
+            // Litera3
+            // 
+            this.Litera3.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera3.Location = new System.Drawing.Point(171, 264);
+            this.Litera3.Name = "Litera3";
+            this.Litera3.Size = new System.Drawing.Size(70, 84);
+            this.Litera3.TabIndex = 4;
+            this.Litera3.Text = "_";
+            // 
+            // Litera4
+            // 
+            this.Litera4.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera4.Location = new System.Drawing.Point(247, 264);
+            this.Litera4.Name = "Litera4";
+            this.Litera4.Size = new System.Drawing.Size(76, 84);
+            this.Litera4.TabIndex = 5;
+            this.Litera4.Text = "_";
+            // 
+            // Litera5
+            // 
+            this.Litera5.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera5.Location = new System.Drawing.Point(329, 264);
+            this.Litera5.Name = "Litera5";
+            this.Litera5.Size = new System.Drawing.Size(75, 84);
+            this.Litera5.TabIndex = 6;
+            this.Litera5.Text = "_";
+            // 
+            // Litera6
+            // 
+            this.Litera6.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera6.Location = new System.Drawing.Point(410, 264);
+            this.Litera6.Name = "Litera6";
+            this.Litera6.Size = new System.Drawing.Size(78, 84);
+            this.Litera6.TabIndex = 7;
+            this.Litera6.Text = "_";
+            // 
+            // Litera7
+            // 
+            this.Litera7.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Litera7.Location = new System.Drawing.Point(494, 264);
+            this.Litera7.Name = "Litera7";
+            this.Litera7.Size = new System.Drawing.Size(64, 84);
+            this.Litera7.TabIndex = 8;
+            this.Litera7.Text = "_";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 386);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 44);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(166, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 66);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Sprawdź";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(604, 82);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(502, 507);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(58, 458);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(462, 66);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 75);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Gra w Wisielca";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 626);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Litera7);
+            this.Controls.Add(this.Litera6);
+            this.Controls.Add(this.Litera5);
+            this.Controls.Add(this.Litera4);
+            this.Controls.Add(this.Litera3);
+            this.Controls.Add(this.Litera2);
+            this.Controls.Add(this.Litera1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Litera1;
+        private System.Windows.Forms.Label Litera2;
+        private System.Windows.Forms.Label Litera3;
+        private System.Windows.Forms.Label Litera4;
+        private System.Windows.Forms.Label Litera5;
+        private System.Windows.Forms.Label Litera6;
+        private System.Windows.Forms.Label Litera7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GraWisielec/wisielec/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wisielec
+{
+    public partial class Form1 : Form
+    {
+        string slowo;
+        int ile_pudel = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            LosujSlowo();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //przycisk sprawdź
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (ile_pudel < 5)
+            {
+                string litera = textBox1.Text;
+                bool czy_trafiony = false;
+                int gdzie_trafiony = 10;// jeżeli jest ustawione na 0 to pętla się psuje i zmienia pierwszą literę.
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if (Convert.ToString(slowo[i]) == litera)
+                    {
+                        czy_trafiony = true;
+                        gdzie_trafiony = i;
+                    }
+                    if (gdzie_trafiony == 0) { Litera1.Text = litera; }
+                    if (gdzie_trafiony == 1) { Litera2.Text = litera; }
+                    if (gdzie_trafiony == 2) { Litera3.Text = litera; }
+                    if (gdzie_trafiony == 3) { Litera4.Text = litera; }
+                    if (gdzie_trafiony == 4) { Litera5.Text = litera; }
+                    if (gdzie_trafiony == 5) { Litera6.Text = litera; }
+                    if (gdzie_trafiony == 6) { Litera7.Text = litera; }
+                }
+
+                if (czy_trafiony == false)
+                {
+                    ile_pudel = ile_pudel + 1;
+                    if (ile_pudel == 1) { pictureBox1.Image = wisielec.Properties.Resources._1; }
+                    if (ile_pudel == 2) { pictureBox1.Image = wisielec.Properties.Resources._2; }
+                    if (ile_pudel == 3) { pictureBox1.Image = wisielec.Properties.Resources._3; }
+                    if (ile_pudel == 4) { pictureBox1.Image = wisielec.Properties.Resources._4; }
+                    if (ile_pudel == 5) {
+                        pictureBox1.Image = wisielec.Properties.Resources.przegrana;
+                        button2.Visible = true;
+                    }
+                }
+                wygrana();
+            }
+            else
+            {
+                pictureBox1.Image = wisielec.Properties.Resources.przegrana;
+            }
+        }
+        
+        private void LosujSlowo()
+        {
+            //tablica z słowami zagadki
+            string[] slowa = { "krokusy", "liliput", "marchew", "selerek", "krakers", "klakier", "ciastka","pstrągi" };
+            int ile_slow = slowa.Length;
+
+            // generetor do losowania słowa zagadki z tablicy
+            Random gen = new Random();
+            int indeks_slowa = gen.Next(0, ile_slow);
+            slowo = slowa[indeks_slowa];
+
+            //generator losowych liczb do wyznaczania losowego miejsca odkrytych liter
+                //litera_1
+            Random gen1 = new Random();
+            int litera_1 = gen1.Next(0, 2);
+                //litera_2
+            Random gen2 = new Random();
+            int litera_2 = gen2.Next(3, 6);
+
+            //ustalenie która litera ma się pokazać w podpowidzi
+                //litera_1
+            if (litera_1 == 0) {Litera1.Text = Convert.ToString(slowo[0]);}
+            if (litera_1 == 1) {Litera2.Text = Convert.ToString(slowo[1]);}
+            if (litera_1 == 2) {Litera3.Text = Convert.ToString(slowo[2]);}
+                //litera_2
+            if (litera_2 == 3) {Litera4.Text = Convert.ToString(slowo[3]);}
+            if (litera_2 == 4) {Litera5.Text = Convert.ToString(slowo[4]);}
+            if (litera_2 == 5) {Litera6.Text = Convert.ToString(slowo[5]);}
+            if (litera_2 == 6) {Litera7.Text = Convert.ToString(slowo[6]);}
+
+            //Litera1.Text = Convert.ToString(slowo[0]);
+            //Litera7.Text = Convert.ToString(slowo[6]);
+
+        }
+        private void wygrana()
+        {
+            if(Litera1.Text != "_")
+            { 
+                if(Litera2.Text != "_")
+                {
+                    if (Litera3.Text != "_")
+                    {
+                        if (Litera4.Text != "_")
+                        {
+                            if (Litera5.Text != "_")
+                            {
+                                if (Litera6.Text != "_")
+                                {
+                                    if (Litera7.Text != "_")
+                                    {
+                                        pictureBox1.Image = wisielec.Properties.Resources.wygrana;
+                                        button2.Visible = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //restart button
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //ustawienie domyślengo znaku 
+            Litera1.Text = "_";
+            Litera2.Text = "_";
+            Litera3.Text = "_";
+            Litera4.Text = "_";
+            Litera5.Text = "_";
+            Litera6.Text = "_";
+            Litera7.Text = "_";
+            //wylosowanie słowa zagadki
+            LosujSlowo();
+            //reset ilości pudeł oraz ukrycie przycisku "reset" oraz wyczyszczenie picturebox1
+            ile_pudel = 0;
+            button2.Visible = false;
+            pictureBox1.Image = null;
+        }
+    }
+}
